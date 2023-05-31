6db37a28a9620b52ce6780996b0edc7c831b887c
realcapobambino <charliealienchamp@gmail.com>
2023-05-31 03:08:44 +0300

    first commit

---
+++ resources/js/Layouts/TwitterLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { router } from "@inertiajs/vue3";
+
+import Twitter from "vue-material-design-icons/Twitter.vue";
+import Magnify from "vue-material-design-icons/Magnify.vue";
+import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
+import Feather from "vue-material-design-icons/Feather.vue";
+import Close from "vue-material-design-icons/Close.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import Earth from "vue-material-design-icons/Earth.vue";
+import ImageOutline from "vue-material-design-icons/ImageOutline.vue";
+import FileGifBox from "vue-material-design-icons/FileGifBox.vue";
+import Emoticon from "vue-material-design-icons/Emoticon.vue";
+import ArrowLeft from "vue-material-design-icons/ArrowLeft.vue";
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <Head title="Welcome" />
+    <TwitterLayout>
+        <h1 class="text-3xl text-center font-extrabold">Hello World!!</h1>
+    </TwitterLayout>
+</template>
+
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import TwitterLayout from "@/Layouts/TwitterLayout.vue";
+</script>
+
+<style></style>
